e1dd1dc3f9feff1c3829e371c571cb92335b4b6c
Jim Smiley <jim.r.smiley@gmail.com>
2014-11-07 22:49:15 -0500

    updated file permissions

--- bin/fix_permissions.bash
+++ bin/fix_permissions.bash
@@ -1,4 +1,4 @@
-chgrp -R $HOME www-data
+chgrp -R www-data $HOME 
 find $HOME/what-hood/app -type f -exec chmod 644 {} \;
 find $HOME/what-hood/app -type d -exec chmod 755 {} \;
 find $HOME/what-hood/app/data/DoctrinORMModule -type d -exec chmod 775 {} \;
