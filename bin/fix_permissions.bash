93ebefdaa366f8b4b5256a6956d8bea7e488a015
Jim Smiley <jim.r.smiley@gmail.com>
2014-11-07 22:33:33 -0500

    created script to fix file permissions

---
+++ bin/fix_permissions.bash
@@ -0,0 +1,5 @@
+chgrp -R $HOME www-data
+find $HOME/what-hood/app -type f -exec chmod 644 {} \;
+find $HOME/what-hood/app -type d -exec chmod 755 {} \;
+find $HOME/what-hood/app/data/DoctrinORMModule -type d -exec chmod 775 {} \;
+chmod 755 $HOME/what-hood/app/logs
