f23bf0d3c81c05401849c97654a0cd3e15952339
Jim Smiley <jim.r.smiley@gmail.com>
2014-11-08 07:05:16 -0500

    more ordering in app/scripts/sql

--- app/scripts/sql/create_neighborhood_polygons_v1.sql
+++
@@ -1,7 +0,0 @@
---
--- using left outer join gets the last heat map created for that neighborhood
---
-SELECT * FROM test_point tp1
-LEFT OUTER JOIN test_point tp2
-    ON ( tp1.strength_of_identity < tp2.strength_of_identity )
-;
