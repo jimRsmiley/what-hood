55bb48136e8b725b90236b0824b37d106f96ddeb
Jim Smiley <jim.r.smiley@gmail.com>
2014-11-30 14:42:30 -0500

    updated to use new function polygon_counts

--- app/scripts/sql/whathood.function.merge_user_polygons.sql
+++ app/scripts/sql/whathood.function.merge_user_polygons.sql
@@ -1,28 +1,55 @@
-DROP FUNCTION IF EXISTS whathood.merge_user_polygons( neighborhood_name text, test_points geometry[] );
+--
+--  merge_user_polygons will merge all of the user polgyons given by _neighborhood_id 
+--  and wrap up test_points into a border where those test points are dominated by _neighborhood_id
+--
 
-DROP TYPE IF EXISTS merge_user_polygon_res_holder;
-CREATE TYPE merge_user_polygon_res_holder AS ( p geometry, num1 int);
+--
+-- drop the function if it exists
+--
+DROP FUNCTION IF EXISTS whathood.merge_user_polygons( 
+  test_points geometry[],
+  neighborhood_id integer
+);
 
+--
+-- create the function whathood.merge_user_polygons
+--
 CREATE OR REPLACE FUNCTION  whathood.merge_user_polygons( 
-  neighborhood_name text, 
-  test_points geometry[]
+  test_points geometry[],
+  _neighborhood_id integer 
 )
-RETURNS SETOF merge_user_polygon_res_holder
+RETURNS polygon_counts_result[]
 AS
 $$
   DECLARE
-    tp geometry;
+    _test_point geometry;
     r merge_user_polygon_res_holder%rowtype;
+    _ret_val polygon_counts_result[];
+    _polygon_counts_result polygon_counts_result;
   BEGIN
   
-    FOREACH tp IN ARRAY test_points LOOP
-      --FOR r IN SELECT tp as test_point, COUNT(*) as count1, 345::int as count2 FROM user_polygon WHERE ST_Contains(polygon,tp) = 'true' LOOP
-      FOR r IN SELECT ST_Point(1,1), 12 as num1 LOOP
-         RETURN NEXT r;
-      END LOOP;
+    FOREACH _test_point IN ARRAY test_points LOOP
+      SELECT whathood.polygon_counts(_test_point,_neighborhood_id) INTO _polygon_counts_result;
+      _ret_val := _polygon_counts_result;
+--      END LOOP;
+--FOR r IN 
+--        SELECT
+--          ST_AsText(_test_point),
+--          cast(_test_point as geometry),
+--          COUNT(*) as count1
+--        FROM user_polygon 
+--        WHERE 
+--          ST_Contains(polygon,_test_point) = 'true'
+--          AND neighborhood_id = _neighborhood_id 
+--      LOOP
+      --FOR r IN SELECT ST_Point(1,1), 12 as num1 LOOP
+--        IF r.num_user_polygons > 0 THEN
+--          RETURN NEXT r;
+--        END IF;
+--      END LOOP;
     END LOOP;
 
-    RETURN;
+    RETURN _ret_val;
   END;
 $$
 LANGUAGE plpgsql;
