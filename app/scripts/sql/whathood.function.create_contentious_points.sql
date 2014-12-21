cbbdd7ad66d7e0e0219cba09a138655cb977a62c
Jim Smiley <jim.r.smiley@gmail.com>
2014-12-21 05:46:47 -0500

    fixed whitespaces

--- app/scripts/sql/whathood.function.create_contentious_points.sql
+++ app/scripts/sql/whathood.function.create_contentious_points.sql
@@ -1,23 +1,23 @@
-﻿ 
+﻿
 --
 -- find the points that are an area of contention
 --
-CREATE OR REPLACE FUNCTION whathood.create_contentious_points( _create_event_id integer ) 
+CREATE OR REPLACE FUNCTION whathood.create_contentious_points( _create_event_id integer )
 RETURNS VOID
 AS
 $BODY$
 BEGIN
   INSERT INTO contentious_point( create_event_id, test_point_id, point, strength_of_identity ) (
-    SELECT DISTINCT     
+    SELECT DISTINCT
       _create_event_id,
       test_point.id,
       test_point.point,
       a.strength_of_identity
-    FROM 
-      neighborhood_point_strength_of_identity a, 
+    FROM
+      neighborhood_point_strength_of_identity a,
       neighborhood_point_strength_of_identity b,
       test_point
-    
+
     WHERE
       test_point.id = a.test_point_id
       AND  a.id <> b.id
