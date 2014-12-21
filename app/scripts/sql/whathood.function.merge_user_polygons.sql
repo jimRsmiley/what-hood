25045aa6d1238534c9ba2d2bb1484d7efffe895e
Jim Smiley <jim.r.smiley@gmail.com>
2014-12-20 19:23:30 -0500

    sql merge_user_polygons updated

--- app/scripts/sql/whathood.function.merge_user_polygons.sql
+++ app/scripts/sql/whathood.function.merge_user_polygons.sql
@@ -1,22 +1,27 @@
 --
---  merge_user_polygons will merge all of the user polgyons given by _neighborhood_id 
+--  merge_user_polygons will merge all of the user polgyons given by _neighborhood_id
 --  and wrap up test_points into a border where those test points are dominated by _neighborhood_id
 --
 
 --
 -- drop the function if it exists
 --
-DROP FUNCTION IF EXISTS whathood.merge_user_polygons( 
+DROP FUNCTION IF EXISTS whathood.gather_test_point_counts(
   test_points geometry[],
   neighborhood_id integer
 );
 
 --
--- create the function whathood.merge_user_polygons
+-- name: whathood.merge_user_polygons
 --
-CREATE OR REPLACE FUNCTION  whathood.merge_user_polygons( 
+-- description: given an array of test points, return:
+--   an array of custom types that
+--   a count the number of user polygons identified by _neighborhood_id
+--   a count of all the user polygons the test point touches
+--
+CREATE OR REPLACE FUNCTION  whathood.gather_test_point_counts(
   test_points geometry[],
-  _neighborhood_id integer 
+  _neighborhood_id integer
 )
 RETURNS SETOF polygon_counts_result
 AS
@@ -27,11 +32,34 @@ $$
     _polygon_count_result_array polygon_counts_result[];
     _polygon_counts_result polygon_counts_result;
   BEGIN
-  
+
     FOREACH _test_point IN ARRAY test_points LOOP
       SELECT * INTO _r FROM whathood.polygon_counts(_test_point,_neighborhood_id);
       RETURN NEXT _r;
    END LOOP;
+  END;
+$$
+LANGUAGE plpgsql;
+
+
+DROP FUNCTION IF EXISTS whathood.merge_user_polygons(
+  _test_points geometry[],
+  _neighborhood_id integer
+);
+
+
+CREATE OR REPLACE FUNCTION whathood.merge_user_polygons(
+  _test_points geometry[],
+  _neighborhood_id integer
+)
+RETURNS integer
+AS
+$$
+  DECLARE
+    _max integer;
+  BEGIN
+    SELECT COUNT(a.*) INTO _max FROM (SELECT * FROM whathood.gather_test_point_counts(_test_points,_neighborhood_id)) a;
+    RETURN _max;
   END;
 $$
 LANGUAGE plpgsql;
