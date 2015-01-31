4447cf5153244a83c7b3fa764ad7bcdaa4857241
Jim Smiley <jim.r.smiley@gmail.com>
2015-01-31 11:45:06 -0500

    created Whathood/Utils module for scripts

---
+++ libs/perl/Whathood/Utils.pm
@@ -0,0 +1,20 @@
+package Whathood::Utils;
+
+use strict;
+use warnings;
+
+sub prompt_user {
+    my $msg = shift;
+    print "$msg. Press CTRL-C to cancel, any key to continue\n";
+    <STDIN>;
+}
+
+sub check_run_as_root {
+    my $user=`whoami`;
+    chomp $user;
+    unless ( $user eq "root" ) {
+       print "must be run as root. Not '$user'\n";
+       exit;
+    }
+}
+1;
