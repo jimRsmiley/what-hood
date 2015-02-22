0c98518f93e2591f0c3328df37e4ef04e1ca63e0
Jim Smiley <jim.r.smiley@gmail.com>
2015-02-22 11:55:49 -0500

    schema whathood-002 - updated to correctly edit whathood_user table

--- app/scripts/schema/whathood-002.sql
+++ app/scripts/schema/whathood-002.sql
@@ -21,33 +21,35 @@ ALTER TABLE neighborhood_polygon ADD CONSTRAINT FK_9A443078803BB24B FOREIGN KEY 
 COMMENT ON COLUMN neighborhood_polygon.geom IS '(DC2Type:geometry)';
 
 -- whathood_user
-ALTER TABLE whathood_user ADD COLUMN ip_address text;
-ALTER table whathood.whathood_user drop column user_name;
+ALTER table whathood_user DROP column user_name;
+ALTER TABLE whathood_user DROP COLUMN facebook_user_id;
+ALTER TABLE whathood_user ADD COLUMN ip_address CHARACTER VARYING(255);
+ALTER TABLE whathood_user ALTER COLUMN ip_address DROP DEFAULT;
+ALTER TABLE whathood_user ALTER COLUMN ip_address SET NOT NULL;
+CREATE UNIQUE INDEX UNIQ_9E2AFB1622FFD58C ON whathood_user (ip_address);
+CREATE UNIQUE INDEX whathood_user_idx ON whathood_user (user_name);
 
+-- user_polygon
 CREATE INDEX IDX_9FA93F18803BB24B ON user_polygon (neighborhood_id);
 CREATE INDEX IDX_9FA93F1898260155 ON user_polygon (region_id);
 CREATE INDEX IDX_9FA93F185219EBCC ON user_polygon (whathood_user_id);
 COMMENT ON COLUMN user_polygon.polygon IS '(DC2Type:polygon)';
-
-ALTER TABLE neighborhood ADD CONSTRAINT FK_FEF1E9EE98260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
-CREATE INDEX IDX_FEF1E9EE98260155 ON neighborhood (region_id);
-CREATE UNIQUE INDEX name_region_idx ON neighborhood (name, region_id);
-
-ALTER TABLE region DROP COLUMN center_point;
-ALTER TABLE region DROP COLUMN border;
-CREATE UNIQUE INDEX region_name_idx ON region (name);
-
 ALTER TABLE user_polygon ADD COLUMN is_deleted BOOLEAN DEFAULT NULL;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F18803BB24B FOREIGN KEY (neighborhood_id) REFERENCES neighborhood (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F1898260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F185219EBCC FOREIGN KEY (whathood_user_id) REFERENCES whathood_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-ALTER TABLE whathood_user DROP facebook_user_id;
-ALTER TABLE whathood_user ALTER ip_address TYPE VARCHAR(255);
-ALTER TABLE whathood_user ALTER ip_address DROP DEFAULT;
-ALTER TABLE whathood_user ALTER ip_address SET NOT NULL;
-CREATE UNIQUE INDEX UNIQ_9E2AFB1622FFD58C ON whathood_user (ip_address);
+-- neighborhood
+ALTER TABLE neighborhood ADD CONSTRAINT FK_FEF1E9EE98260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
+CREATE INDEX IDX_FEF1E9EE98260155 ON neighborhood (region_id);
+CREATE UNIQUE INDEX name_region_idx ON neighborhood (name, region_id);
 
+-- region
+ALTER TABLE region DROP COLUMN center_point;
+ALTER TABLE region DROP COLUMN border;
+CREATE UNIQUE INDEX region_name_idx ON region (name);
+
+-- ZfcUser stuff
 CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1;
 CREATE SEQUENCE role_id_seq INCREMENT BY 1 MINVALUE 1 START 1;
 CREATE TABLE users (id INT NOT NULL, username VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, displayName VARCHAR(50) DEFAULT NULL, password VARCHAR(128) NOT NULL, PRIMARY KEY(id));
@@ -63,6 +65,13 @@ ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8EA76ED395 FOREIGN KEY (user_id)
 ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8ED60322AC FOREIGN KEY (role_id) REFERENCES role (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE role ADD CONSTRAINT FK_57698A6A727ACA70 FOREIGN KEY (parent_id) REFERENCES role (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 
+-- up_np
+CREATE TABLE up_np (np_id INT NOT NULL, up_id INT NOT NULL, PRIMARY KEY(np_id, up_id));
+CREATE INDEX IDX_50B466A6121F828F ON up_np (np_id);
+CREATE INDEX IDX_50B466A652F241C ON up_np (up_id);
+CREATE UNIQUE INDEX IDX_up_np_pair ON up_np(np_id,up_id);
+
+-- insert ZfcUser into
 INSERT INTO role
     (id, parent_id, roleId)
 VALUES
@@ -70,12 +79,3 @@ VALUES
     (2, 1, 'user'),
     (3, 2, 'moderator'),
     (4, 3, 'administrator');
-
-CREATE TABLE whathood_user (id INT NOT NULL, facebook_user_id BIGINT DEFAULT NULL, user_name VARCHAR(255) NOT NULL, PRIMARY KEY(id));
-CREATE UNIQUE INDEX whathood_user_idx ON whathood_user (user_name);
-
-
-CREATE TABLE up_np (np_id INT NOT NULL, up_id INT NOT NULL, PRIMARY KEY(np_id, up_id));
-CREATE INDEX IDX_50B466A6121F828F ON up_np (np_id);
-CREATE INDEX IDX_50B466A652F241C ON up_np (up_id);
-CREATE UNIQUE INDEX IDX_up_np_pair ON up_np(np_id,up_id);
