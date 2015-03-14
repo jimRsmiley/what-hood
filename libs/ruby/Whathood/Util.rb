08f57331ae53cf74c67029f0e31eacb89244bcf9
Jim Smiley <jim.r.smiley@gmail.com>
2015-03-14 10:21:52 -0400

    delete the current code on a deploy

--- libs/ruby/Whathood/Util.rb
+++ libs/ruby/Whathood/Util.rb
@@ -6,5 +6,18 @@ module Whathood
             puts "executing #{sql_stmt}"
             puts `sudo -u postgres psql --tuples-only -c "#{sql_stmt}" #{db_name}`
         end
+
+		def self.check_for_root_user()
+			username = ENV['USER']
+
+			if username != 'root' and username != 'vagrant'
+				abort 'must run script as root'
+			end
+		end
+
+		def self.prompt_user(str)
+			print str
+			option = gets
+		end
     end
 end
