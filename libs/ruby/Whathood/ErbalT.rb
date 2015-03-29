4b46b7a7be519b8ba63772052c8a6e738b4a6fd4
Jim Smiley <jim.r.smiley@gmail.com>
2015-03-29 12:01:20 -0400

    added ruby Whathood::ErbalT.rb

---
+++ libs/ruby/Whathood/ErbalT.rb
@@ -0,0 +1,13 @@
+require 'ostruct'
+
+module Whathood
+    class ErbalT < OpenStruct
+        def self.render_from_hash(t, h)
+            ErbalT.new(h).render(t)
+        end
+
+        def render(template)
+            ERB.new(template).result(binding)
+        end
+    end
+end
