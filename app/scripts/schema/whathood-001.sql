d2532a08b6e3538d21f57d39bd13cddbd432e0aa
Jim Smiley <jim.r.smiley@gmail.com>
2015-04-05 10:51:05 -0400

    add neighborhood.no_build_border to schema update

--- app/scripts/schema/whathood-001.sql
+++ app/scripts/schema/whathood-001.sql
@@ -8,6 +8,26 @@ DROP TABLE neighborhood_polygons_create_event CASCADE;
 -- this is a long table drop
 --DROP TABLE neighborhood_point_strength_of_identity;
 
+-- neighborhood
+ALTER TABLE neighborhood ADD CONSTRAINT FK_FEF1E9EE98260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE neighborhood ADD COLUMN no_build_border boolean DEFAULT false;
+CREATE INDEX IDX_FEF1E9EE98260155 ON neighborhood (region_id);
+CREATE UNIQUE INDEX name_region_idx ON neighborhood (name, region_id);
+
+-- region
+ALTER TABLE region DROP COLUMN center_point;
+ALTER TABLE region DROP COLUMN border;
+CREATE UNIQUE INDEX region_name_idx ON region (name);
+
+-- whathood_user
+ALTER table whathood_user DROP column user_name;
+ALTER TABLE whathood_user DROP COLUMN facebook_user_id;
+ALTER TABLE whathood_user ADD COLUMN ip_address CHARACTER VARYING(255);
+ALTER TABLE whathood_user ALTER COLUMN ip_address DROP DEFAULT;
+ALTER TABLE whathood_user ALTER COLUMN ip_address SET NOT NULL;
+CREATE UNIQUE INDEX UNIQ_9E2AFB1622FFD58C ON whathood_user (ip_address);
+CREATE UNIQUE INDEX whathood_user_idx ON whathood_user (user_name);
+
 -- neighborhood polygon
 TRUNCATE TABLE neighborhood_polygon;
 ALTER TABLE neighborhood_polygon DROP COLUMN num_user_polygons;
@@ -20,15 +40,6 @@ CREATE INDEX IDX_9A443078803BB24B ON neighborhood_polygon (neighborhood_id);
 ALTER TABLE neighborhood_polygon ADD CONSTRAINT FK_9A443078803BB24B FOREIGN KEY (neighborhood_id) REFERENCES neighborhood (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 COMMENT ON COLUMN neighborhood_polygon.geom IS '(DC2Type:geometry)';
 
--- whathood_user
-ALTER table whathood_user DROP column user_name;
-ALTER TABLE whathood_user DROP COLUMN facebook_user_id;
-ALTER TABLE whathood_user ADD COLUMN ip_address CHARACTER VARYING(255);
-ALTER TABLE whathood_user ALTER COLUMN ip_address DROP DEFAULT;
-ALTER TABLE whathood_user ALTER COLUMN ip_address SET NOT NULL;
-CREATE UNIQUE INDEX UNIQ_9E2AFB1622FFD58C ON whathood_user (ip_address);
-CREATE UNIQUE INDEX whathood_user_idx ON whathood_user (user_name);
-
 -- user_polygon
 CREATE INDEX IDX_9FA93F18803BB24B ON user_polygon (neighborhood_id);
 CREATE INDEX IDX_9FA93F1898260155 ON user_polygon (region_id);
@@ -38,16 +49,6 @@ ALTER TABLE user_polygon ADD COLUMN is_deleted BOOLEAN DEFAULT NULL;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F18803BB24B FOREIGN KEY (neighborhood_id) REFERENCES neighborhood (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F1898260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE user_polygon ADD CONSTRAINT FK_9FA93F185219EBCC FOREIGN KEY (whathood_user_id) REFERENCES whathood_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
-
--- neighborhood
-ALTER TABLE neighborhood ADD CONSTRAINT FK_FEF1E9EE98260155 FOREIGN KEY (region_id) REFERENCES region (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
-CREATE INDEX IDX_FEF1E9EE98260155 ON neighborhood (region_id);
-CREATE UNIQUE INDEX name_region_idx ON neighborhood (name, region_id);
-
--- region
-ALTER TABLE region DROP COLUMN center_point;
-ALTER TABLE region DROP COLUMN border;
-CREATE UNIQUE INDEX region_name_idx ON region (name);
 
 -- ZfcUser stuff
 CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1;
@@ -70,6 +71,27 @@ CREATE TABLE up_np (np_id INT NOT NULL, up_id INT NOT NULL, PRIMARY KEY(np_id, u
 CREATE INDEX IDX_50B466A6121F828F ON up_np (np_id);
 CREATE INDEX IDX_50B466A652F241C ON up_np (up_id);
 CREATE UNIQUE INDEX IDX_up_np_pair ON up_np(np_id,up_id);
+
+DROP INDEX IF EXISTS neighborhood_polygon_polygon_idx;
+CREATE INDEX neighborhood_polygon_polygon_idx ON neighborhood_polygon USING GIST(polygon);
+
+DROP INDEX IF EXISTS heat_map_test_point_idx;
+CREATE INDEX heat_map_test_point_idx ON heat_map_test_point USING GIST(point);
+
+DROP INDEX IF EXISTS region_polygon_idx;
+CREATE INDEX region_polygon_idx ON region USING GIST(polygon);
+
+DROP INDEX IF EXISTS user_polygon_neighborhood_id_idx;
+CREATE INDEX user_polygon_neighborhood_id_idx ON user_polygon(neighborhood_id);
+
+-- user polygon
+CREATE INDEX user_polygon_polygon_idx ON user_polygon USING GIST(polygon);
+
+CREATE INDEX test_point_id_idx ON my_temp_table(test_point_id);
+CREATE INDEX neighborhood_name_idx ON my_temp_table(neighborhood_id);
+CREATE UNIQUE INDEX neighborhood_name_test_point_id_idx ON my_temp_table(test_point_id,neighborhood_id);
+CREATE UNIQUE INDEX test_point_set_num_test_point_idx ON test_point(set_num,point);
+VACUUM ANALYZE;
 
 -- insert ZfcUser into
 INSERT INTO role
