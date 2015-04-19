5469cd0803acf8c526543ae4e0438aea79ad4a4d
Jim Smiley <jim.r.smiley@gmail.com>
2015-04-19 13:27:55 +0000

    lib/ruby/Whathood/Util - added method check_for_just_root

--- libs/ruby/Whathood/Util.rb
+++ libs/ruby/Whathood/Util.rb
@@ -7,11 +7,20 @@ module Whathood
             puts `sudo -u postgres psql --tuples-only -c "#{sql_stmt}" #{db_name}`
         end
 
+        # just check for root and not vagrant
+		def self.check_for_just_root_user()
+			username = ENV['USER']
+
+			if username != 'root'
+				abort 'must run script as root'
+			end
+		end
+
 		def self.check_for_root_user()
 			username = ENV['USER']
 
 			if username != 'root' and username != 'vagrant'
-				abort 'must run script as root'
+				abort 'must run script as root or vagrant'
 			end
 		end
 
