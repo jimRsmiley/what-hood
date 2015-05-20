3d0acc2159d4fce6ae150a683b4b4ab00bf29961
Jim Smiley <jim.r.smiley@gmail.com>
2015-05-20 00:30:06 +0000

    W/Mapper/PointsAsPolygonMapper - got pgr_pointsAsPolygon to build neighborhood borders

--- app/module/Whathood/src/Whathood/Mapper/PointsAsPolygonMapper.php
+++ app/module/Whathood/src/Whathood/Mapper/PointsAsPolygonMapper.php
@@ -6,22 +6,36 @@ use Doctrine\ORM\Query\ResultSetMapping;
 use Whathood\Spatial\PHP\Types\Geometry\MultiPoint;
 use Whathood\Spatial\PHP\Types\Geometry\Polygon;
 
+/**
+ *
+ *  - create a temp table
+ * first store the multi_point in a temp table so that it can be called by pgr_pointsAsPolygon
+ *
+ */
 class PointsAsPolygonMapper extends BaseMapper {
 
-    public function toPolygon(MultiPoint $multi_point) {
-        $tmp_table = "pap_tmp_tbl";
-        $this->createTempTable($tmp_table);
-        $this->savePoints($multi_point,$tmp_table);
-        $db_val = $this->spatialPlatform()->convertToDatabaseValue($multi_point);
+    protected $_tmp_table_name = "pap_tmp_tbl";
 
+    /**
+     * create a temp table
+     * save the multipoints into it
+     * query the temp table with pgr_pointsAsPolygon to create the neighborhood border
+     **/
+    public function toPolygon(MultiPoint $multi_point) {
+        $this->createTempTable($this->_tmp_table_name);
+        $this->savePoints($multi_point,$this->_tmp_table_name);
+        $polygon = $this->pointsAsPolygon($this->_tmp_table_name);
+        $this->deleteTempTable($this->_tmp_table_name);
+        return $polygon;
+    }
+
+    protected function pointsAsPolygon($tmp_table_name) {
         $alias = 'as_text';
-        $sql = "SELECT ST_AsEWKB(pgr_pointsAsPolygon('SELECT 1::int4 AS id, ST_X(point)::float8 AS x, ST_Y(point)::float8 AS y FROM $tmp_table ')) AS $alias";
+        $sql = "SELECT ST_AsEWKB(pgr_pointsAsPolygon('SELECT 1::int4 AS id, ST_X(point)::float8 AS x, ST_Y(point)::float8 AS y FROM $tmp_table_name '::text)) AS $alias";
 
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult($alias,$alias);
         $query = $this->em->createNativeQuery($sql,$rsm);
-
-        $query->setParameter(':multi_point_as_text',$db_val);
         $result = $query->getSingleResult();
         $sqlExpr = $result[$alias];
 
@@ -30,6 +44,10 @@ class PointsAsPolygonMapper extends BaseMapper {
         return $polygon;
     }
 
+    public function deleteTempTable($tbl_name) {
+        $sql = "DROP TABLE IF EXISTS $tbl_name";
+        $this->em->getConnection()->query($sql);
+    }
     public function createTempTable($tbl_name) {
         $sql = "CREATE TEMP TABLE $tbl_name (point geometry)";
         $this->em->getConnection()->query($sql);
