818d1d63daabbc715aece179183471e798168666
Jim Smiley <jim.r.smiley@gmail.com>
2015-08-05 14:37:45 +0000

    added libs/react

---
+++ libs/react/Whathood.jsx
@@ -0,0 +1,23 @@
+var WhathoodClickResultNeighborhood = React.createClass({
+  render: function() {
+    return (
+      <div>Neighborhood {this.props.name}</div>
+    );
+  }
+});
+
+var WhathoodClickResult = React.createClass({
+  render: function() {
+    var rows = new Array();
+    for (var i = 0; i < this.props.neighborhoods.length; i++) {
+      console.log(this.props.neighborhoods[i]);
+      rows.push(<WhathoodClickResultNeighborhood data={this.props.neighborhoods[i]} />);
+    }
+    return (
+      <div className="whathoodClickResult">
+        {this.props.region.name}
+        {rows}
+      </div>
+    );
+  }
+});
