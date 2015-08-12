d1d4dd45246acd4d4259c81a75177e668e4e9bde
Jim Smiley <jim.r.smiley@gmail.com>
2015-08-12 22:29:35 +0000

    re-add search controller

---
+++ app/module/Whathood/src/Whathood/Controller/SearchController.php
@@ -0,0 +1,41 @@
+<?php
+namespace Whathood\Controller;
+
+use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
+use Whathood\Model\Whathood\WhathoodResult;
+
+/**
+ * class manages the searching for regions and neighborhood names
+ *
+ * @author Jim Smiley twitter:@jimRsmiley
+ */
+class SearchController extends BaseController {
+
+    public function indexAction() {
+        $queryString = $this->getUriParameter('q');
+
+        /*
+         * get regions
+         */
+        try {
+            $regions = $this->m()->regionMapper()->nameLike( $queryString );
+        } catch( \Doctrine\ORM\NoResultException $e ) {
+            $regions = array();
+        }
+
+        try {
+            $neighborhoods = $this->m()->neighborhoodMapper()
+                                            ->nameLike( $queryString );
+        } catch( \Doctrine\ORM\NoResultException $e ) {
+            $neighborhoods = array();
+        }
+
+       $this->logger()->info( "neighbordhood/region search for '$queryString'");
+       return new ViewModel( array(
+            'queryString' => $queryString,
+            'regions' => $regions,
+            'neighborhoods' => $neighborhoods
+        ));
+    }
+}
