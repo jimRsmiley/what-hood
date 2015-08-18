acab52c6ee9bcd7db260dcb41ac123bb09062d17
Jim Smiley <jim.r.smiley@gmail.com>
2015-08-18 12:43:38 +0000

    remove stale code

--- app/module/Whathood/src/Whathood/Controller/Restful/RegionRestController.php
+++ app/module/Whathood/src/Whathood/Controller/Restful/RegionRestController.php
@@ -47,69 +47,6 @@ class RegionRestController extends AbstractRestfulController {
     public function delete($id) {
 
     }
-
-    public function showAction() {
-        $regionName = $this->getUriParameter('region_name');
-        $setNumber = $this->getUriParameter('set_number');
-
-        if( empty( $regionName ) ) {
-            $regionName = 'Philadelphia';
-        }
-
-        if( empty( $setNumber ) )
-            $setNumber = $this->neighborhoodPolygonMapper ()->getLastCreateEventId();
-
-        try {
-            $region = $this->regionMapper()->getRegionByName( $regionName );
-        } catch( \Doctrine\ORM\NoResultException $e ) {
-            $viewModel = new ViewModel( array( 'regionName' => $regionName ) );
-            $viewModel->setTemplate('whathood/region/no-region-by-name.phtml');
-            return $viewModel;
-        }
-
-        $viewModel = $this->getViewModel( array(
-            'region'    => $region,
-            'setNumber' => $setNumber
-            )
-        );
-        $viewModel->setTemplate('whathood/region/region-show.phtml');
-        return $viewModel;
-    }
-
-    public function listRegionsAction() {
-
-        $mapper = $this->getServiceLocator()
-                ->get('Whathood\Mapper\RegionMapper');
-
-        $regions = $mapper->fetchAll();
-
-        return new ViewModel( array( 'regions' => $regions ) );
-    }
-
-    public function editAction() {
-        $regionName = $this->getEvent()->getRouteMatch()->getParam('region');
-
-        $neighborhoods = $this->neighborhoodMapper()
-                                ->getAuthoritativeNeghborhoodsByRegionName($regionName);
-
-        \Zend\Debug\Debug::dump($neighborhoods);
-        exit;
-        $userName = $neighborhoods[0]->getUser()->getName();
-
-        $viewModel = new ViewModel(
-                array(
-                    'neighborhoods' => $neighborhoods,
-                    'regionName'    => $regionName,
-                    'userName'      => $userName
-                )
-            );
-        $viewModel->setTemplate('whathood/region/region-show.phtml');
-        return $viewModel;
-    }
-
-    public function getRegionMapper() {
-        return $this->getServiceLocator()->get('Whathood\Mapper\RegionMapper');
-    }
 }
 
 ?>
