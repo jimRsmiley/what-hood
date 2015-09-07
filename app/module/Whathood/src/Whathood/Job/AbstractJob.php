02d03a86f7d6c9a331e9845abaa5f1b73f8cf947
Jim Smiley <jim.r.smiley@gmail.com>
2015-09-07 20:59:09 +0000

    W/Job/AbstractJob - have a name

--- app/module/Whathood/src/Whathood/Job/AbstractJob.php
+++ app/module/Whathood/src/Whathood/Job/AbstractJob.php
@@ -4,15 +4,29 @@ namespace Whathood\Job;
 abstract class AbstractJob extends \SlmQueue\Job\AbstractJob {
 
     protected $_logger = null;
-
-    public function setLogger($logger) {
-        $this->_logger = $logger;
-    }
-
-    public function logger() { return $this->_logger; }
+    protected $_name = null;
 
     public function __construct(array $data) {
         $hydrator = new \Zend\Stdlib\Hydrator\ClassMethods(true);
         $hydrator->hydrate($data,$this);
     }
+
+    public function getName() {
+        if (!$this->_name) {
+            $reflect = new \ReflectionClass($this);
+            $this->_name = strtolower($reflect->getShortName());
+        }
+        return $this->_name;
+    }
+
+    public function setLogger($logger) {
+        $this->_logger = $logger;
+    }
+
+    public function infoLog($str) {
+        print "$str\n";
+    }
+
+    public function logger() { return $this->_logger; }
+
 }
