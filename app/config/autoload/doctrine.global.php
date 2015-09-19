187ee6a17087b55ff23147f346b9e0363bfcd205
Jim Smiley <jim.r.smiley@gmail.com>
2015-09-19 22:26:30 +0000

    consolidate doctrine config

--- app/config/autoload/doctrine.global.php
+++ app/config/autoload/doctrine.global.php
@@ -5,6 +5,14 @@ return array(
         'connection' => array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOPgSql\Driver',
+                'params' => array(
+                    'host'     =>  (getenv('PGHOST') ? getenv('PGHOST') : 'wh-postgis'),
+                    'port'     => '5432',
+                    'dbname'   => 'whathood',
+                    'user'     => 'docker',
+                    # we don't allow remote sql connections
+                    'password' => null
+                ),
                 'doctrine_type_mappings' => array(
                     'geometry' => 'geometry',
                     'polygon'  => 'polygon',
