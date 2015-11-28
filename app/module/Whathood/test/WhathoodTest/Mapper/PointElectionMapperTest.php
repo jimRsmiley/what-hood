a19843e720c2c262fdc55567082bcfb62e8b6ee5
James Smiley <james.robert.smiley@gmail.com>
2015-11-28 19:09:36 +0000

    exclude doctrine mapping cache test dir

--- app/module/Whathood/test/WhathoodTest/Mapper/PointElectionMapperTest.php
+++ app/module/Whathood/test/WhathoodTest/Mapper/PointElectionMapperTest.php
@@ -5,19 +5,5 @@ namespace WhathoodTest\Mapper;
 class PointElectionMapperTest extends \Whathood\PHPUnit\BaseTest {
 
     public function testGenerateBorderPolygonTest() {
-
-        $up1 = UserBorder::build();
-        $up2 = ;
-        $up3 = ;
-        $neighborood = new Neighborhood();
-
-        $election_collection = new Collection();
-
-        $mapper = \Whathood\Mapper\PointElectionMapper();
-
-        $border = $mapper->generateBorderPolygon(
-            $election_collection, $neighborhood->getId() );
-
-        \Zend\Debug\Debug::dump($border);
     }
 }
