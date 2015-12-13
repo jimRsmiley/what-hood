933683ba3ffc8777af7f52ee5d7ad0b0acd11cae
James Smiley <james.robert.smiley@gmail.com>
2015-12-13 14:17:48 -0500

    add buildFromParent

--- app/module/Whathood/src/Whathood/Spatial/PHP/Types/Geometry/Polygon.php
+++ app/module/Whathood/src/Whathood/Spatial/PHP/Types/Geometry/Polygon.php
@@ -5,10 +5,17 @@ use CrEOF\Spatial\PHP\Types\Geometry\Polygon as CrEOFPolygon;
 
 class Polygon extends CrEOFPolygon {
 
+    /**
+     * build a new Polygon given rings and srid
+     */
     public static function build(array $rings, $srid) {
         if (!$srid)
             throw new \InvalidArgumentException("srid may not be empty");
         return new static($rings, $srid);
     }
+
+    public static function buildFromParent(CrEOFPolygon $parent) {
+        $obj = new static($parent->getRings(), $parent->getSRID());
+        return $obj;
+    } 
 }
-?>
